e0f7fa70e25c3000055ffd2d721bf28642f84863
Hun <dudgns9194@gmail.com>
2024-12-10 21:24:29 +0900

    GetMapping 어노테이션을 활용

--- src/main/java/com/example/testproject/controller/HelloController.java
+++ src/main/java/com/example/testproject/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.testproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hello")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("hello")
     public String hello(){
         return "hello World";
     }
