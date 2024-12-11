002d45830d1c821bee0f21069251604dcc4766a3
Hun <dudgns9194@gmail.com>
2024-12-11 22:32:07 +0900

    post api

---
+++ src/main/java/com/example/testproject/controller/PostController.java
@@ -0,0 +1,35 @@
+package com.example.testproject.controller;
+
+import com.example.testproject.dto.MemberDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @PostMapping(value = "/default")
+    public String pstMethod() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+}
